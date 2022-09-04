eeb83825c554474c6d4efe5ebd66f4fb3ca0ea0c
DaxoDax <90693294+DaxoDax@users.noreply.github.com>
2022-09-04 13:49:31 +0200

    EndOfSection2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+     
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;   
+        }
+
+        //zasto actionresult, jer zelimo da nam se vrati neki action result u kojem stavimo da ce tip toga bit lista
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
